39533fb7872f1044e92b368bae621318dd6c0d42
JeslynnChan <chloe.chan73@gmail.com>
2022-09-04 22:08:07 +0800

    Update reducer.js

--- js/reducer.js
+++ js/reducer.js
@@ -2,9 +2,7 @@ export default function reducer(state, action) {
     switch (action.type){
         case 'SET_DATA' : {
             const r2Data = action.value
-            return (
-                <Text />
-            )
+            return
         }
     }
 }

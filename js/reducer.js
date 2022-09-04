69f48e0857a13919a92c549a04e7d7d1e1e8165a
JeslynnChan <chloe.chan73@gmail.com>
2022-09-04 22:06:23 +0800

    Create reducer.js

---
+++ js/reducer.js
@@ -0,0 +1,10 @@
+export default function reducer(state, action) {
+    switch (action.type){
+        case 'SET_DATA' : {
+            const r2Data = action.value
+            return (
+                <Text />
+            )
+        }
+    }
+}
